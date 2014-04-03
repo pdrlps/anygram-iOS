25e3e763244ea08a60c05bab670d3c59d060303d
Pedro Lopes <hello@pedrolopes.net>
2014-04-03 11:27:59 +0100

    Initial Commit

---
+++ Anygram/LOPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOPAppDelegate.h
+//  Anygram
+//
+//  Created by Pedro Lopes on 03/04/14.
+//  Copyright (c) 2014 Pedro Lopes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
